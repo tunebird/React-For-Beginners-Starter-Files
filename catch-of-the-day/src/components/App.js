8757a8bd1e28644f75c23424a8346d8d044ebaa0
Andrew Watson <andy@watsons.ch>
2017-04-17 16:55:18 +0200

    add samples and list of fish
    use smaller components to display each individual fish, use a sample button to preload the state with a bunch of default values

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 	constructor() {
 		super();
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+
 		// getinitialState
 		this.state = {
 			fishes: {},
@@ -24,14 +28,27 @@ class App extends React.Component {
 		this.setState({ fishes })
 	}
 
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market"/>
+					<ul className="list-of-fishes">
+						{
+							Object
+							.keys(this.state.fishes)
+							.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+						}
+					</ul>
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
