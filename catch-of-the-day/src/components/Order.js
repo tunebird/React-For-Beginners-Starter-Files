4c42e5d4045a8f979599f34f30ad04b49beba27b
Andrew Watson <andy@watsons.ch>
2017-04-18 21:45:51 +0200

    add some cool animations
    using CSS Transition Group to animate items in and out

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { formatPrice } from '../helpers'
+import CSSTransitionGroup from 'react-addons-css-transition-group';
+
 
 class Order extends React.Component {
   constructor() {
@@ -19,9 +21,20 @@ class Order extends React.Component {
 
     return (
       <li key={key}>
-        <span>{count}lbs {fish.name}</span>
+        <span>
+          <CSSTransitionGroup
+            component="span"
+            className="count"
+            transitionName="count"
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={250}
+          >
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+          lbs {fish.name} {removeButton}
+        </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
-        {removeButton}
+        
       </li>
     )
   }
@@ -40,13 +53,21 @@ class Order extends React.Component {
 		return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+
+
+        <CSSTransitionGroup 
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           {orderIds.map(this.renderOrder)}
           <li className="total">
           <strong>Total:</strong>
           {formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
 
       </div>
 		)
